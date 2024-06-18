910a6d1f82d1ebc80d83873c442a67e1fd82b975
ron199807 <ronaldmweema@gmail.com>
2024-06-17 18:12:39 -0700

    added a python file

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,9 @@
 #!/bin/bash
 number1 = 10
 number2 = 5
-print("addition of 10 and 5 is" number1 + number2)
-print("subtraction of 10 and 5 is" number1 - number2)
-print("multiplication of 10 and 5 is" number1 * number2)
+sum1 = namer1 + number2
+subtraction = number1 - number2
+product =  number1 * number2
+print("addition of 10 and 5 is" sum1)
+print("subtraction of 10 and 5 is" subtraction)
+print("multiplication of 10 and 5 is" product)
