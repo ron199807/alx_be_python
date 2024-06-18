5b37dfb817601d386c6b3545fb743a89070eca24
ron199807 <ronaldmweema@gmail.com>
2024-06-17 17:59:57 -0700

    modified a file

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,6 @@
 #!/bin/bash
 number1 = 10
 number2 = 5
-print("addition of" number1 "and" number2 "is" number1 + number2)
-print("subtraction of" number1 "and" number2 "is" number1 - number2)
-print("multiplication of" number1 "and" number2 "is" number1 * number2)
+print("addition of 10 and 5 is" number1 + number2)
+print("subtraction of 10 and 5 is" number1 - number2)
+print("multiplication of 10 and 5 is" number1 * number2)
