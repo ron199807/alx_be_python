1dee27bbb9b912ec97456d0d2bee4972dc953e98
ron199807 <ronaldmweema@gmail.com>
2024-06-17 18:49:04 -0700

    add a python file

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,13 @@
+#define the variables
 number1 = 10
 number2 = 5
-sum1 = namer1 + number2
+
+#perform the calculations
+sum1 = namber1 + number2
 subtraction = number1 - number2
 product =  number1 * number2
-print("Addition of" number1 "and" number2 "is" sum1)
-print("Subtraction of" number1 "and" number2 "is" subtraction)
-print("Multiplication of" number1 "and"number2 "is" product)
+
+# print the results
+print("Addition of", number1, "and", number2, "is", sum1)
+print("Subtraction of", number1, "and", number2, "is", subtraction)
+print("Multiplication of", number1, "and", number2, "is", product)
