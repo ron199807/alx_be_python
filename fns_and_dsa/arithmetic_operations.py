878cd0739dd85a053cadd220a04c9f5f646b0329
ron199807 <ronaldmweema@gmail.com>
2024-07-03 03:06:31 -0700

    added a arithmetic_operations. py file

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,20 @@
+def perform_operation(num1, num2, operation):
+    """
+    num1 (float):
+    num2 (float):
+    operation (str):
+    """
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "Error: divided by zero"
+        else:
+            return num1 / num2
+    else:
+        return "Error: Invalid operation"
+
