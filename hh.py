647e48fa7ecddee9f1b316d171782a5a86f77f6f
ron199807 <ronaldmweema@gmail.com>
2024-07-06 16:28:19 +0200

    modified hh.py

---
+++ hh.py
@@ -0,0 +1,6 @@
+def who_you_are():
+    name = input("what is you name: ")
+    age = int(input("how old are you: "))
+    print(f"Your name is {name} and you are {age} years old.")
+
+who_you_are()
