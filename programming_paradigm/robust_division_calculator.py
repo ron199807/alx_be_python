150edb1c94997711c13e6482e737dd6ec2c60053
ron199807 <ronaldmweema@gmail.com>
2024-07-09 12:21:19 +0200

    added a robust_division_calculator.py

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,19 @@
+def safe_divide(numerator, denominator):
+    try:
+        numerator = float(numerator)
+        denominator = float(denominator)
+
+        if denominator == 0:
+            raise ZeroDivisionError("Denominator cannot be zero.")
+        return numerator / denominator
+    except ZeroDivisionError as e:
+        print(f"Error: {e}")
+        return None
+    except ValueError:
+        return "Error: both inputs must be numeric."
+    except Exception as e:
+        return f"Unexpected error: {e}"
+
+
+
+           
