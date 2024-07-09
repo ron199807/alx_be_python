cbd6fdfe8aee56a057a3b2217a9ee7dde00f87bf
ron199807 <ronaldmweema@gmail.com>
2024-07-09 12:32:21 +0200

    added a robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -10,7 +10,7 @@ def safe_divide(numerator, denominator):
         print(f"Error: {e}")
         return None
     except ValueError:
-        return "Error: Please enter numeric values only"
+        return "Error: Please enter numeric values only."
     except Exception as e:
         return f"Unexpected error: {e}"
 
