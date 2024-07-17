8f9b819cfe1b7e2479e6cb4ebc79de8dc79d38a0
ron199807 <ronaldmweema@gmail.com>
2024-07-17 04:14:02 +0200

    first commit in oop

--- oop/book_class.py
+++ oop/book_class.py
@@ -12,5 +12,5 @@ class Book:
         return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self):
-        return f"Book('{self.title}', '{self.author}', '{self.year}')"
+        return f"Book('{self.title}', '{self.author}', {self.year})"
         
